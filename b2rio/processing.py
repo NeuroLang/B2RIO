c3c1e33e26060253d704ee162f259f971faacf02
Gaston Zanitti <gzanitti@gmail.com>
2022-07-04 15:49:40 +0200

    Single region analysis

--- b2rio/processing.py
+++ b2rio/processing.py
@@ -16,40 +16,17 @@ class Solver():
         self.mni_t1 = image.resample_img(mni_t1, np.eye(3) * resample)
 
 
-    def run(self, regions_ids, regions_name_path, brain_path, probabilistic_atlas=False, results_path='./'):
-
-        lines = []
-        with open(regions_name_path) as f:
-            lines = f.readlines()
-
-        count = 0
-        res = []
-        for line in lines:
-            if count == 0:
-                count += 1
-                continue
-            splited = line.split(' ')
-            res.append((splited[0], ' '.join(splited[1:-1])[1:], splited[-1][:-2]))
-
-        regions = pd.DataFrame(res, columns=['r_number', 'r_name', 'hemis'])
-        regions = regions.astype({'r_number': 'int64'})
+    def run(self, brain_image, radius=4):
 
         pmaps_4d = image.resample_img(
-            image.load_img(brain_path), self.mni_t1.affine, interpolation='nearest'
+            brain_image, self.mni_t1.affine, interpolation='nearest'
         )
 
         brain_regions_prob = []
-        for r in regions.r_number.values.astype(int):
-            prob_region_data = pmaps_4d.dataobj[:,:,:,r-1]
-            non_zero = np.nonzero(prob_region_data)
-            for x, y, z in zip(*non_zero):
-                if probabilistic_atlas:
-                    p = float(prob_region_data[x][y][z])
-                    d = (p, x, y, z, r)
-                else:
-                    #p = int(prob_region_data[x][y][z])
-                    d = (x, y, z, r)
-                brain_regions_prob.append(d)
+        non_zero = np.nonzero(pmaps_4d.dataobj)
+        for x, y, z, _ in zip(*non_zero):
+            d = (x, y, z)
+            brain_regions_prob.append(d)
 
         ns_database_fn, ns_features_fn = datasets.utils._fetch_files(
             datasets.utils._get_dataset_dir('neurosynth'),
@@ -117,25 +94,16 @@ class Solver():
         )
         doc_folds = nl.add_tuple_set(ns_doc_folds, name='doc_folds')
 
-
-        activations = nl.add_tuple_set(ns_database.values, name='activations')
+        ns_data = ns_database[ns_database.space == 'MNI'][['id', 'i', 'j', 'k']].values
+        activations = nl.add_tuple_set(ns_data, name='activations')
         terms = nl.add_tuple_set(ns_terms.values, name='terms')
         docs = nl.add_uniform_probabilistic_choice_over_set(
                 ns_docs.values, name='docs'
         )
 
-        terms_det = nl.add_tuple_set(
-            ns_terms.term.unique(), name='terms_det'
-        )
-
-        j_brain = nl.add_tuple_set(
+        nl.add_tuple_set(
             brain_regions_prob,
             name='julich_brain_det'
-        )
-
-        j_regions = nl.add_tuple_set(
-            regions.values,
-            name='julich_regions'
         )
 
         with nl.scope as e:
@@ -149,81 +117,57 @@ class Solver():
                 (e.term == word_lower[e.onto_term])
             )
 
-            e.filtered_terms[e.d, e.t] = (
+            e.f_terms[e.d, e.t] = (
                 e.terms[e.d, e.t] &
                 e.lower_terms[e.t]
             )
 
-            f_term = nl.query((e.d, e.t), e.filtered_terms(e.d, e.t))
+            f_term = nl.query((e.d, e.t), e.f_terms(e.d, e.t))
 
         filtered = f_term.as_pandas_dataframe()
-        filtered_terms = nl.add_tuple_set(filtered.values, name='filtered_terms')
+        nl.add_tuple_set(filtered.values, name='filtered_terms')
 
-        for id_region in regions_ids:
-            try:
-                with nl.scope as e:
-                    #if probabilistic_atlas:
-                    (e.jbd @ e.p)[e.i, e.j, e.k, e.region, e.d] = (
-                        e.activations[
-                            e.d, ..., ..., ..., ..., 'MNI', ..., ..., ..., ...,
-                            ..., ..., ..., e.i, e.j, e.k
-                        ] &
-                        e.julich_brain_det[e.p, e.i, e.j, e.k, e.region] &
-                        (e.region == id_region)
+        try:
+            with nl.scope as e:
+                e.jbd[e.x, e.y, e.z, e.d] = (
+                    e.activations[e.d, 'MNI', e.x, e.y, e.z] &
+                    e.julich_brain_det[e.x1, e.y1, e.z1] &
+                    (e.dist == e.EUCLIDEAN(e.x, e.y, e.z, e.x1, e.y1, e.z1)) &
+                    (e.dist < radius)
+                )
+
+                e.img_studies[e.d] = e.jbd[..., ..., ..., e.d]
+
+                e.img_left_studies[e.d] = e.docs[e.d] & ~(e.img_studies[e.d])
+
+                e.term_prob[e.t, e.fold, e.PROB[e.t, e.fold]] = (
+                    (
+                        e.filtered_terms[e.d, e.t]
+                    ) // (
+                        e.img_studies[e.d] &
+                        e.doc_folds[e.d, e.fold] &
+                        e.docs[e.d]
                     )
-                    '''else:
-                        e.jbd[e.i, e.j, e.k, e.region, e.d] = (
-                            e.activations[
-                                e.d, ..., ..., ..., ..., 'MNI', ..., ..., ..., ...,
-                                ..., ..., ..., e.i, e.j, e.k
-                            ] &
-                            e.julich_brain_det[e.i, e.j, e.k, e.region] &
-                            (e.region == id_region)
-                        )
-                    '''
+                )
 
-                    e.act_regions[e.d, e.id] = e.jbd[..., ..., ..., e.id, e.d]
-
-
-                    e.no_act_regions[e.d, e.id] = (
-                        ~(e.act_regions[e.d, e.id]) &
-                        e.docs[e.d] &
-                        e.julich_regions[e.id, ..., ...]
+                e.no_term_prob[e.t, e.fold, e.PROB[e.t, e.fold]] = (
+                    (
+                        e.filtered_terms[e.d, e.t]
+                    ) // (
+                        e.img_left_studies[e.d] &
+                        e.doc_folds[e.d, e.fold] &
+                        e.docs[e.d]
                     )
+                )
 
-                    e.term_prob[e.t, e.fold, e.PROB[e.t, e.fold]] = (
-                        (
-                            e.filtered_terms[e.d, e.t]
-                        ) // (
-                            e.act_regions[e.d, id_region] &
-                            e.doc_folds[e.d, e.fold] &
-                            e.docs[e.d]
-                        )
-                    )
+                e.ans[e.term, e.fold, e.bf] = (
+                    e.term_prob[e.term, e.fold, e.p] &
+                    e.no_term_prob[e.term, e.fold, e.pn] &
+                    (e.bf == (e.p / e.pn))
+                )
 
-                    e.no_term_prob[e.t, e.fold, e.PROB[e.t, e.fold]] = (
-                        (
-                            e.filtered_terms[e.d, e.t]
-                        ) // (
-                            e.no_act_regions[e.d, id_region] &
-                            e.doc_folds[e.d, e.fold] &
-                            e.docs[e.d]
-                        )
-                    )
+                res = nl.query((e.term, e.fold, e.bf), e.ans[e.term, e.fold, e.bf])
+        except Exception as e:
+            print(f'ERROR! : {e}')
 
-                    e.ans[e.t, e.f, e.bf, e.p, e.pn] = (
-                        e.term_prob[e.t, e.f, e.p] &
-                        e.no_term_prob[e.t, e.f, e.pn] &
-                        (e.bf == (e.p / e.pn))
-                    )
-
-                    res = nl.query((e.t, e.f, e.bf, e.p, e.pn), e.ans[e.t, e.f, e.bf, e.p, e.pn])
-
-                    pss = res.as_pandas_dataframe()
-                    pss.to_hdf(f'{results_path}neuro_paper_ri_term_probs_region{id_region}.hdf', key=f'results')
-
-            except Exception as e:
-                print(f'Failed on region: {id_region}')
-                print(e)
-                #pss = pd.DataFrame([], columns=['t', 'fold', 'PROB'])
-                #pss.to_hdf(f'reverse_inference_results/neuro_paper_ri_term_probs_region{id_region}_{n_folds}folds_no_resample.hdf', key=f'results')
+        return res
