abfff4b149c9ff24ea61a6a7f8babe87b817cbfd
Gaston Zanitti <gzanitti@gmail.com>
2022-07-18 14:11:31 +0200

    Drop unnamed column

--- b2rio/b2rio.py
+++ b2rio/b2rio.py
@@ -223,6 +223,7 @@ def run():
             return
 
         df = res.as_pandas_dataframe()
+        df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
         df_cp = f_term.as_pandas_dataframe()[['cp', 't']]
         df_cp = df_cp.drop_duplicates()
@@ -460,12 +461,13 @@ def run_probabilistic():
             return
 
         df = res.as_pandas_dataframe()
+        df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
         df_cp = f_term.as_pandas_dataframe()[['cp', 't']]
         df_cp = df_cp.drop_duplicates()
 
         df = df.set_index('term').join(df_cp.set_index('t'))
-        df.reset_index(inplace=True)
+        df.reset_index(inplace=True, drop=True)
         df = df.rename(columns={'cp': 'topConcept'})
 
         if len(regions2analyse) > 1:
